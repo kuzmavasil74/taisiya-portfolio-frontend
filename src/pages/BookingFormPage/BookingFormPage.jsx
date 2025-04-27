4a870d9a65315e08eca6ea59faa77a4a2a0dfe24
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2025-04-27 10:01:13 +0200

    add_booking_order

--- src/pages/BookingFormPage/BookingFormPage.jsx
+++ src/pages/BookingFormPage/BookingFormPage.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
-import BookingForm from '../../components/BookingForm/BookingForm'
 import styles from './BookingFormPage.module.css'
 import { useTranslation } from 'react-i18next'
+
 function BookingFormPage() {
   const { t } = useTranslation()
+  const services = [
+    { key: 'haircuts', link: 'https://calendly.com/kuzmavasil-v/30min' },
+    { key: 'maleHaircuts', link: 'https://calendly.com/kuzmavasil-v/30min' },
+    { key: 'keratin', link: 'https://calendly.com/kuzmavasil-v/new-meeting' },
+    {
+      key: 'hotBotox',
+      link: 'https://calendly.com/kuzmavasil-v/new-meeting-1',
+    },
+    {
+      key: 'coldRestoration',
+      link: 'https://calendly.com/kuzmavasil-v/new-meeting-1',
+    },
+    {
+      key: 'coldBotox',
+      link: 'https://calendly.com/kuzmavasil-v/new-meeting-1',
+    },
+    { key: 'polishing', link: 'https://calendly.com/kuzmavasil-v/30min' },
+  ]
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>{t('bookingForm.title')}</h2>
-      <BookingForm />
+      <div className={styles.buttonContainer}>
+        {services.map((service) => (
+          <button
+            key={service.key}
+            className={styles.button}
+            onClick={() => (window.location.href = service.link)}
+          >
+            {t(`bookingForm.${service.key}`)}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
--- src/utills/validationBookingSchema.js
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup'
-
-export const validationBookingSchema = Yup.object({
-  name: Yup.string().required('Name is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  phone: Yup.string().required('Phone is required'),
-  date: Yup.string().required('Date is required'),
-  coments: Yup.string(),
-})
